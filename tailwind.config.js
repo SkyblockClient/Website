635d1dc7a2cf1343bf0ea4aebd0236226e190bfa
Kendell R <KTibow@users.noreply.github.com>
2022-09-24 11:32:07 -0700

    Revamp the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: ["**/*.html", "**/*.js"],
+  content: ["docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-4": "#8ff03f",
+        "main-3": "#70cb44",
+        "main-2": "#56a644",
+        "main-1": "#42823f",
+        "main-0": "#336037",
+      },
+    },
   },
   plugins: [],
 };
